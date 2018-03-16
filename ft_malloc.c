f1dafcb36406fe46bb22828db366ed7a79cd2610
Simon GAUDIN <simongaudin65@gmail.com>
2018-03-16 16:43:43 +0100

    beggining

--- ft_malloc.c
+++ ft_malloc.c
@@ -2,18 +2,32 @@
 
 void *ft_malloc(size_t size)
 {
-	if (size == 0 || !init_zones())
+	if (size <= 0 || !init_zones())
 		return (NULL);
-	printf("zones successfully created !\n");
 
-	return NULL;
+	t_head *head = mmap(0, sizeof(t_head), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (!head)
+		return NULL;
+	head->size = size;
+
+	void *mem = mmap(head + sizeof(t_head), size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (mem == (void *) -1)
+		return NULL;
+
+	printf("Head address = %p\n", head);
+	printf("Mem address in malloc = %p\n", mem);
+	printf("size stocked in malloc : %d\n", (int)head->size);
+
+	return mem;
 }
 
 int main()
 {
-	ft_malloc(1);
+	char *str = NULL;
 
-	ft_malloc(2);
+	str = ft_malloc(10);
+	if (str == NULL)
+		printf("NULL\n");
 
 	return 0;
 }
--- malloc.h
+++ malloc.h
@@ -3,26 +3,43 @@
 
 # include <unistd.h>
 # include <sys/mman.h>
+# include <stdlib.h>
 
 #include <stdio.h>
 
-# define TINY_ZONE 4096 * 100
-# define SMALL_ZONE 4096 * 2 * 100
+# define TINY_ZONE 1024 * 100
+# define SMALL_ZONE TINY_ZONE * 100
 
-typedef struct 		s_mem
+enum zoneType
 {
-	struct s_mem	*prev;
+	TINY,
+	SMALL,
+	LARGE
+}
+
+typedef struct 		s_head
+{
+	size_t			size;
+	size_t			spaceBeforeNext;
 	void			*mem;
 	struct s_mem	*next;
-}					t_mem;
+}					t_head;
 
-typedef struct 		s_prealloc
+typedef struct 		s_map
 {
-	t_mem			*tiny;
-	t_mem			*small;
-}					t_prealloc;
+	size_t			available_space;
+	zoneType		type;
+	s_head			*next;
+}					t_map;
 
-t_prealloc 			*zones;
+typedef struct 		s_zones
+{
+	t_map			tiny;
+	t_map			small;
+	t_map			large;
+}					t_zones;
+
+t_zones 			*zones;
 
 int					init_zones();
 int 				pushback_mem(t_mem *zone);
